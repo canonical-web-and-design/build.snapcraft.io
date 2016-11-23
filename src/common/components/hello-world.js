2ab835f4a843b9c792843aedd53e71f577cb8601
Katie Fenn <katie.fenn@googlemail.com>
2016-11-23 09:35:06 +0000

    Added temporary Hello World route

---
+++ src/common/components/hello-world.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class HelloWorld extends Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default HelloWorld;
