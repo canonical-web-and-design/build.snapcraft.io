2bc06f0d0cdcac1dc237aa7c15cdc84e5c3fa1b3
Bartek Szopka <bartaz@users.noreply.github.com>
2016-11-23 15:44:20 +0100

    Added simple home container (#6)
    * Added simple home container
    * Added proper configuration for CSS Modules require hook

---
+++ src/common/containers/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+
+import HelloWorld from '../components/hello-world';
+
+import styles from './container.css';
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div className={ styles.container }>
+        <Helmet
+          title='Home'
+        />
+        <HelloWorld />
+      </div>
+    );
+  }
+}
--- src/common/routes.js
+++ src/common/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from './containers/app.js';
-import HelloWorld from './components/hello-world';
+import Home from './containers/home.js';
 
 export default (
   <Route component={App}>
-    <Route path="/" component={HelloWorld}/>
+    <Route path="/" component={Home}/>
   </Route>
 );
