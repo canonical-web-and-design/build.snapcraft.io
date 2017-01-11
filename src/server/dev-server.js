e18eb2a90aab2fa244e2c2fe6feeefd9921924ea
Bartek Szopka <bartaz@users.noreply.github.com>
2017-01-11 13:57:55 +0100

    Fix dev images URL (#89)

--- src/server/dev-server.js
+++ src/server/dev-server.js
@@ -3,7 +3,7 @@ const clearRequireCache = require('./helpers/hot-load').clearRequireCache;
 const chokidar = require('chokidar');
 
 const conf = require('./helpers/config').conf;
-const WEBPACK_DEV_URL = conf.get('SERVER:WEBPACK_DEV_URL') || '';
+const WEBPACK_DEV_URL = conf.get('WEBPACK_DEV_URL') || '';
 require('images-require-hook')('.svg', `${WEBPACK_DEV_URL}/static/icons`);
 
 const app = require('./server').default;
