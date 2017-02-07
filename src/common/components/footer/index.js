9bb5c12df5abcabcfe97f1b7c91f807a60c275b4
Bartek Szopka <bartaz@users.noreply.github.com>
2017-02-07 14:13:32 +0100

    Updated footer with legal info and social icons (#110)

--- src/common/components/footer/index.js
+++ src/common/components/footer/index.js
@@ -7,8 +7,23 @@ export default class Footer extends Component {
     return (
       <div className={ styles.footer }>
         <div className={ styles.container }>
+          <div className={ styles.social }>
+            <h3 className={ styles.heading }>Follow us</h3>
+            <ul className={ styles.socialList }>
+              <li className={ styles.socialListItem }>
+                <a href="https://twitter.com/snapcraftio" className={ styles.socialListItemTwitter }>Share on Twitter</a>
+              </li>
+              <li className={ styles.socialListItem }>
+                <a href="https://plus.google.com/+SnapcraftIo" className={ styles.socialListItemGoogle }>Share on Google plus</a>
+              </li>
+              <li className={ styles.socialListItem }>
+                <a href="https://www.facebook.com/snapcraftio" className={ styles.socialListItemFacebook }>Share on Facebook</a>
+              </li>
+            </ul>
+          </div>
           <p className={ styles.copyright }>© 2017 Canonical Ltd. Ubuntu and Canonical are registered trademarks of Canonical Ltd.</p>
           <p><a href="http://www.ubuntu.com/legal">Legal information</a> · <a href="https://github.com/canonical-ols/build.snapcraft.io/issues/new">Report a bug on this site</a></p>
+          <p className={ styles.note }>Yocto Project and all related marks and logos are trademarks of The Linux Foundation. This website is not, in any way, endorsed by the Yocto Project or The Linux Foundation.</p>
         </div>
       </div>
     );
