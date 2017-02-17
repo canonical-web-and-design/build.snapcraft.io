053f6ee8dafd97504ca1b6e274c7daa6b182fd02
Bartek Szopka <bartaz@users.noreply.github.com>
2017-02-17 17:19:19 +0100

    Updated heading components and vanilla base h* styles (#161)

--- src/common/components/vanilla/heading/index.js
+++ src/common/components/vanilla/heading/index.js
@@ -1,9 +1,50 @@
-import HeadingOne from './heading-one';
-import HeadingTwo from './heading-two';
-import HeadingThree from './heading-three';
-import HeadingFour from './heading-four';
-import HeadingFive from './heading-five';
-import HeadingSix from './heading-six';
+import React, { PropTypes } from 'react';
+
+import styles from './heading.css';
+
+const Heading = (props) => {
+  const H = props.heading || 'h1';
+  return (
+    <H className={ styles[H] }>
+      { props.children }
+    </H>
+  );
+};
+
+Heading.propTypes = {
+  children: PropTypes.node,
+  heading: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+};
+
+const HeadingOne = (props) => Heading({
+  ...props,
+  heading: 'h1'
+});
+
+const HeadingTwo = (props) => Heading({
+  ...props,
+  heading: 'h2'
+});
+
+const HeadingThree = (props) => Heading({
+  ...props,
+  heading: 'h3'
+});
+
+const HeadingFour = (props) => Heading({
+  ...props,
+  heading: 'h4'
+});
+
+const HeadingFive = (props) => Heading({
+  ...props,
+  heading: 'h5'
+});
+
+const HeadingSix = (props) => Heading({
+  ...props,
+  heading: 'h6'
+});
 
 export {
   HeadingOne,
