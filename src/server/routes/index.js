e56bafd3dcee0f04591e325e04c0f64776b7c642
Colin Watson <cjwatson@ubuntu.com>
2017-02-21 10:08:29 +0000

    Fix store route import (#171)

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,7 @@ import github from './github';
 import githubAuth from './github-auth';
 import launchpad from './launchpad';
 import login from './login';
-import store from 'store';
+import store from './store';
 import universal from './universal';
 import webhook from './webhook';
 
