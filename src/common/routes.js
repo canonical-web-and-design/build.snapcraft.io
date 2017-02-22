339ef07b475d34f4011b6895abf435448ae8ea6f
Stephen <stephen.stewart@carisenda.com>
2017-02-22 17:18:28 +0000

    redirect unauthed users to / (#203)
    * redirect unauthed users to /
    
    * we don't need to store the pre redirect url, but nice to know how it works

--- src/common/routes.js
+++ src/common/routes.js
@@ -8,12 +8,15 @@ import Landing from './containers/landing.js';
 import LoginFailed from './containers/login-failed.js';
 import RepositorySetup from './containers/repository-setup.js';
 import SelectRepositories from './containers/select-repositories.js';
+import EnsureLoggedIn from './containers/ensure-logged-in';
 
 export default (
   <Route component={App}>
     <Route path="/" component={Landing}/>
-    <Route path="/dashboard" component={Dashboard}/>
-    <Route path="/dashboard/select-repositories" component={SelectRepositories}/>
+    <Route component={ EnsureLoggedIn }>
+      <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/dashboard/select-repositories" component={SelectRepositories}/>
+    </Route>
     <Route path="/:owner/:name/setup" component={RepositorySetup}/>
     <Route path="/:owner/:name/builds" component={Builds}/>
     <Route path="/:owner/:name/builds/:buildId" component={BuildDetails}/>
