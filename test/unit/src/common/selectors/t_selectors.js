d76d0aa370f12c4605f4b0d7eff2a0fba8b80cca
Stephen Stewart <stephen.stewart@carisenda.com>
2017-03-13 12:04:08 +0000

    drop memoize tests

--- test/unit/src/common/selectors/t_selectors.js
+++ test/unit/src/common/selectors/t_selectors.js
@@ -2,7 +2,7 @@ import expect from 'expect';
 
 import { hasNoRegisteredNames } from '../../../../../src/common/selectors';
 
-describe('hasNoRegisteredNames', function() {
+describe('hasNoRegisteredNames selector', function() {
 
   const stateWithNoName = {
     snaps: {
@@ -18,30 +18,7 @@ describe('hasNoRegisteredNames', function() {
     }
   };
 
-  // XXX must run first in suite to avoid memoizing a selector and being
-  // off-by-one in recomputations()
-  context('is a memoized selector', function() {
-    beforeEach(function() {
-      hasNoRegisteredNames.resetRecomputations();
-    });
-
-    it('and should recompute the value on new state', function() {
-      expect(hasNoRegisteredNames.recomputations()).toBe(0);
-      hasNoRegisteredNames(stateWithName);
-      expect(hasNoRegisteredNames.recomputations()).toBe(1);
-      hasNoRegisteredNames(stateWithNoName);
-      expect(hasNoRegisteredNames.recomputations()).toBe(2);
-    });
-
-    it('and should not recompute the value on same state', function() {
-      hasNoRegisteredNames(stateWithNoName);
-      hasNoRegisteredNames(stateWithNoName);
-      hasNoRegisteredNames(stateWithNoName);
-      expect(hasNoRegisteredNames.recomputations()).toBe(0);
-    });
-  });
-
-  context('that checks if any registered names in state', function() {
+  context('checks if any registered names are in state', function() {
     it('should be false when no names in state', function() {
       expect(hasNoRegisteredNames(stateWithNoName)).toBe(true);
     });
