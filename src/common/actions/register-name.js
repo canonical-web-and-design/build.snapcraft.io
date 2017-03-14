9add8b0e4d7b8920f983f539f47ca96b4376f1b8
Colin Watson <cjwatson@ubuntu.com>
2017-03-14 15:00:59 +0000

    Convert remaining actions to async/await (#471)

--- src/common/actions/register-name.js
+++ src/common/actions/register-name.js
@@ -19,40 +19,42 @@ export const REGISTER_NAME_CLEAR = 'REGISTER_NAME_CLEAR';
 const STORE_SERIES = '16';
 const STORE_CHANNELS = ['edge'];
 
-export function getPackageUploadRequestMacaroon() {
-  return localforage.getItem('package_upload_request')
-    .catch(() => null)
-    .then((packageUploadRequest) => {
-      if (packageUploadRequest === null) {
-        throw new APICompatibleError({
-          code: 'not-logged-into-store',
-          message: 'Not logged into store',
-          detail: 'No package_upload_request macaroons in local storage'
-        });
-      }
-      let macaroons;
-      try {
-        macaroons = checkPackageUploadRequest(
-          packageUploadRequest.root, packageUploadRequest.discharge
-        );
-      } catch (e) {
-        throw new APICompatibleError({
-          code: 'not-logged-into-store',
-          message: 'Not logged into store',
-          detail: `Checking package_upload_request macaroons failed: ${e}`
-        });
-      }
-      const root = macaroons.root.serialize();
-      const discharge = MacaroonsBuilder.modify(macaroons.root)
-        .prepare_for_request(macaroons.discharge)
-        .getMacaroon()
-        .serialize();
-      return { root, discharge };
+export async function getPackageUploadRequestMacaroon() {
+  let packageUploadRequest;
+  try {
+    packageUploadRequest = await localforage.getItem('package_upload_request');
+  } catch (error) {
+    packageUploadRequest = null;
+  }
+  if (packageUploadRequest === null) {
+    throw new APICompatibleError({
+      code: 'not-logged-into-store',
+      message: 'Not logged into store',
+      detail: 'No package_upload_request macaroons in local storage'
     });
+  }
+  let macaroons;
+  try {
+    macaroons = checkPackageUploadRequest(
+      packageUploadRequest.root, packageUploadRequest.discharge
+    );
+  } catch (e) {
+    throw new APICompatibleError({
+      code: 'not-logged-into-store',
+      message: 'Not logged into store',
+      detail: `Checking package_upload_request macaroons failed: ${e}`
+    });
+  }
+  const root = macaroons.root.serialize();
+  const discharge = MacaroonsBuilder.modify(macaroons.root)
+    .prepare_for_request(macaroons.discharge)
+    .getMacaroon()
+    .serialize();
+  return { root, discharge };
 }
 
-function signAgreement(root, discharge) {
-  return fetch(`${BASE_URL}/api/store/agreement`, {
+async function signAgreement(root, discharge) {
+  const response = await fetch(`${BASE_URL}/api/store/agreement`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -63,21 +65,19 @@ function signAgreement(root, discharge) {
       root,
       discharge
     })
-  })
-    .then((response) => {
-      if (response.status >= 200 && response.status < 300) {
-        return null;
-      } else {
-        return response.json().then((json) => {
-          const payload = json.error_list ? json.error_list[0] : json;
-          throw getError(response, { status: 'error', payload });
-        });
-      }
-    });
+  });
+  if (response.status >= 200 && response.status < 300) {
+    return null;
+  } else {
+    const json = await response.json();
+    const payload = json.error_list ? json.error_list[0] : json;
+    throw getError(response, { status: 'error', payload });
+  }
 }
 
-export function internalRegisterName(root, discharge, snapName, repositoryUrl) {
-  return fetch(`${BASE_URL}/api/store/register-name`, {
+export async function internalRegisterName(root, discharge, snapName,
+                                           repositoryUrl) {
+  const response = await fetch(`${BASE_URL}/api/store/register-name`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -89,24 +89,21 @@ export function internalRegisterName(root, discharge, snapName, repositoryUrl) {
       root,
       discharge
     })
-  })
-    .then((response) => {
-      if (response.status >= 200 && response.status < 300) {
-        return response;
-      } else {
-        return response.json().then((json) => {
-          if (response.status === 409 && json.code === 'already_owned') {
-            return { status: 201 };
-          } else {
-            throw getError(response, { status: 'error', payload: json });
-          }
-        });
-      }
-    });
+  });
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const json = await response.json();
+    if (response.status === 409 && json.code === 'already_owned') {
+      return { status: 201 };
+    } else {
+      throw getError(response, { status: 'error', payload: json });
+    }
+  }
 }
 
-function getPackageUploadMacaroon(root, discharge, snapName) {
-  return fetch(`${conf.get('STORE_API_URL')}/acl/`, {
+async function getPackageUploadMacaroon(root, discharge, snapName) {
+  const response = await fetch(`${conf.get('STORE_API_URL')}/acl/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -118,21 +115,20 @@ function getPackageUploadMacaroon(root, discharge, snapName) {
       permissions: ['package_upload'],
       channels: STORE_CHANNELS
     })
-  })
-    .then((response) => response.json().then((json) => {
-      if (response.status !== 200 || !json.macaroon) {
-        throw new APICompatibleError({
-          code: 'snap-name-not-registered',
-          message: 'Snap name is not registered in the store',
-          snap_name: snapName
-        });
-      }
-      return json.macaroon;
-    }));
+  });
+  const json = await response.json();
+  if (response.status !== 200 || !json.macaroon) {
+    throw new APICompatibleError({
+      code: 'snap-name-not-registered',
+      message: 'Snap name is not registered in the store',
+      snap_name: snapName
+    });
+  }
+  return json.macaroon;
 }
 
 export function registerName(repository, snapName, options={}) {
-  return (dispatch) => {
+  return async (dispatch) => {
     const { fullName } = repository;
 
     dispatch({
@@ -140,46 +136,39 @@ export function registerName(repository, snapName, options={}) {
       payload: { id: fullName, snapName }
     });
 
-    let root;
-    let discharge;
-    let packageUploadMacaroon;
-    return getPackageUploadRequestMacaroon()
-      .then((macaroons) => {
-        ({ root, discharge } = macaroons);
-        if (options.signAgreement) {
-          return signAgreement(root, discharge)
-            .then(() => {
-              return dispatch(getAccountInfo(options.signAgreement, {
-                root, discharge
-              }));
-            });
-        }
-      })
-      .then(() => internalRegisterName(root, discharge, snapName, repository.url))
-      .then(() => getPackageUploadMacaroon(root, discharge, snapName))
-      .then((macaroon) => {
-        packageUploadMacaroon = macaroon;
-        return fetch(`${BASE_URL}/api/launchpad/snaps/authorize`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            repository_url: repository.url,
-            snap_name: snapName,
-            series: STORE_SERIES,
-            channels: STORE_CHANNELS,
-            macaroon: packageUploadMacaroon
-          }),
-          credentials: 'same-origin'
-        });
-      })
-      .then(checkStatus)
-      .then(() => dispatch(registerNameSuccess(fullName, snapName)))
-      .then(() => {
-        if (options.requestBuilds) {
-          return dispatch(requestBuilds(repository.url));
-        }
-      })
-      .catch((error) => dispatch(registerNameError(fullName, error)));
+    try {
+      const { root, discharge } = await getPackageUploadRequestMacaroon();
+      if (options.signAgreement) {
+        await signAgreement(root, discharge);
+        await dispatch(getAccountInfo(options.signAgreement, {
+          root, discharge
+        }));
+      }
+      await internalRegisterName(root, discharge, snapName, repository.url);
+      const packageUploadMacaroon = await getPackageUploadMacaroon(
+        root, discharge, snapName
+      );
+      const authorizeUrl = `${BASE_URL}/api/launchpad/snaps/authorize`;
+      const response = await fetch(authorizeUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          repository_url: repository.url,
+          snap_name: snapName,
+          series: STORE_SERIES,
+          channels: STORE_CHANNELS,
+          macaroon: packageUploadMacaroon
+        }),
+        credentials: 'same-origin'
+      });
+      await checkStatus(response);
+      await dispatch(registerNameSuccess(fullName, snapName));
+      if (options.requestBuilds) {
+        await dispatch(requestBuilds(repository.url));
+      }
+    } catch (error) {
+      dispatch(registerNameError(fullName, error));
+    }
   };
 }
 
