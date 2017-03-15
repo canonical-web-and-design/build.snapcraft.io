490fea8dbdbdd0e70ff1ca63ebc802e6e9571b4d
Stephen <stephen.stewart@carisenda.com>
2017-03-15 08:04:54 +0000

    Fix traffic light proptype validation warning (#483)
    * remove proptype validation for label
    
    * fix flow for all in absense of label

--- src/common/components/traffic-lights/index.js
+++ src/common/components/traffic-lights/index.js
@@ -23,7 +23,7 @@ export class Signal extends Component {
 
     return (
       <div className={ styles.box }>
-        <div className={ classNames(styles.signal, signalStyle) }></div>
+        <div className={ classNames(styles.signal, signalStyle) } />
         <p>{ message }</p>
       </div>
     );
@@ -32,7 +32,6 @@ export class Signal extends Component {
 
 Signal.propTypes = {
   state: PropTypes.number.isRequired,
-  label: PropTypes.string.isRequired,
   message: PropTypes.string
 };
 
