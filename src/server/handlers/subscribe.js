f05eb3cbbda83a3493918321c8e3f888036759e0
Colin Watson <cjwatson@ubuntu.com>
2017-04-12 11:11:02 +0100

    Make MailChimp request through a proxy (#626)
    Part of #617.

--- src/server/handlers/subscribe.js
+++ src/server/handlers/subscribe.js
@@ -2,9 +2,12 @@ import 'isomorphic-fetch';
 import url from 'url';
 import sanitizeHtml from 'sanitize-html';
 
+import { conf } from '../helpers/config';
+
 export const MAILCHIMP_FORM_URL = 'https://canonical.us3.list-manage.com/subscribe/post-json';
 export const MAILCHIMP_FORM_U = '56dac47c206ba0f58ec25f314';
 export const MAILCHIMP_FORM_ID = '381f5c55f1';
+const HTTP_PROXY = conf.get('HTTP_PROXY');
 
 export const privateRepos = async (req, res) => {
   const formUrl = url.parse(MAILCHIMP_FORM_URL);
@@ -22,7 +25,8 @@ export const privateRepos = async (req, res) => {
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
-    }
+    },
+    proxy: HTTP_PROXY
   });
   const json = await response.json();
 
