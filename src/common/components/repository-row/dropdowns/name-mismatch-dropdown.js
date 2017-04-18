569ec41e7f91d874da1dee7e025e66030189d517
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-04-18 14:05:08 +0200

    Moving bits of JSX to helper methods

--- src/common/components/repository-row/dropdowns/name-mismatch-dropdown.js
+++ src/common/components/repository-row/dropdowns/name-mismatch-dropdown.js
@@ -11,49 +11,20 @@ import {
 import styles from './dropdowns.css';
 
 const NameMismatchDropdown = (props) => {
-  const { snapcraft_data, store_name, git_repository_url } = props.snap;
+  const { snapcraft_data, store_name } = props.snap;
 
   let helpText;
 
   if (snapcraft_data.nameOwnershipStatus === NAME_OWNERSHIP_REGISTERED_BY_OTHER_USER) {
-    helpText = (
-      <p>
-        “{snapcraft_data.name}” is registered to someone else. {' '}
-        Probably best to {' '}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={getTemplateUrl(git_repository_url, snapcraft_data.path)}
-        >
-          change snapcraft.yaml
-        </a> to use “{store_name}” instead.
-      </p>
-    );
+    helpText = getNameRegisteredByOtherUserHelpText(props.snap);
   // TODO cover name registered for another repo #299
   // } else if (snapcraft_data.nameOwnershipStatus === NAME_OWNERSHIP_ALREADY_OWNED) {
   //   ...
   // }
-  } else {
-    helpText = (
-      <div className={styles.caption}>
-        <p>You can:</p>
-        <ul>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={getTemplateUrl(git_repository_url, snapcraft_data.path)}
-            >
-              Change snapcraft.yaml
-            </a> to use “{store_name}”
-          </li>
-          <li>
-            <a onClick={props.onOpenRegisterNameClick}>Register the name</a> “{snapcraft_data.name}”
-          </li>
-        </ul>
-      </div>
-    );
+  } else if (snapcraft_data.nameOwnershipStatus) {
+    helpText = getNameMismatchHelpText(props.snap, props.onOpenRegisterNameClick);
   }
+
   return (
     <Dropdown>
       <Row>
@@ -68,6 +39,48 @@ const NameMismatchDropdown = (props) => {
     </Dropdown>
   );
 };
+
+function getNameRegisteredByOtherUserHelpText(snap) {
+  const { snapcraft_data, store_name, git_repository_url } = snap;
+
+  return (
+    <p>
+      “{snapcraft_data.name}” is registered to someone else. {' '}
+      Probably best to {' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={getTemplateUrl(git_repository_url, snapcraft_data.path)}
+      >
+        change snapcraft.yaml
+      </a> to use “{store_name}” instead.
+    </p>
+  );
+}
+
+function getNameMismatchHelpText(snap, onOpenRegisterNameClick) {
+  const { snapcraft_data, store_name, git_repository_url } = snap;
+
+  return (
+    <div className={styles.caption}>
+      <p>You can:</p>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={getTemplateUrl(git_repository_url, snapcraft_data.path)}
+          >
+            Change snapcraft.yaml
+          </a> to use “{store_name}”
+        </li>
+        <li>
+          <a onClick={onOpenRegisterNameClick}>Register the name</a> “{snapcraft_data.name}”
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 NameMismatchDropdown.propTypes = {
   snap: PropTypes.shape({
