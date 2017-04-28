f99c3545845f1297546d6fa23abf2c2d2d9aa752
Adam Collard <sparkiegeek@users.noreply.github.com>
2017-04-28 12:06:45 +0100

    Don't try to use removed store routes (#695)
    Remove the routes that are unnecessary and non-existant

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,6 @@ import github from './github';
 import githubAuth from './github-auth';
 import launchpad from './launchpad';
 import login from './login';
-import store from './store';
 import universal from './universal';
 import webhook from './webhook';
 import subscribe from './subscribe';
@@ -12,7 +11,6 @@ export {
   github,
   launchpad,
   login,
-  store,
   subscribe,
   webhook,
   universal
--- src/server/server.js
+++ src/server/server.js
@@ -61,7 +61,6 @@ app.use(generateToken);
 app.use('/', routes.login);
 app.use('/api', routes.github);
 app.use('/api', routes.launchpad);
-app.use('/api', routes.store);
 app.use('/api', routes.subscribe);
 app.use('/', routes.webhook);
 app.use(routes.githubAuth);
