0ffe33ff1e8343f38a8522dc3dd077d1fb5eca82
Richard McCartney <richard@mccartney.io>
2017-07-06 15:56:36 +0100

    866 create search field component (#873)
    * Adding search field folder and files to components.
    
    * Adding and setup of the search component, and loading into the select repositories page.
    
    * Applying styles to the page header and search to fix sizing and alignment.
    
    * Removing the search component from select repositories page.
    
    * Style and props clean up for the search input component.
    
    * Fixing props spacing.
    
    * Adding search input icon background to component.

---
+++ src/common/components/search-input/index.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import styles from './search.css';
+
+export default function SearchInput(props) {
+  return (
+    <label htmlFor={ props.id }>
+      { props.label }
+      <input
+        id={ props.id }
+        className={ styles.searchInput }
+        onChange={ props.onChange }
+        placeholder={ props.placeholder }
+        value={ props.value }
+        type='search'
+      />
+    </label>
+  );
+}
+
+SearchInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired
+};
