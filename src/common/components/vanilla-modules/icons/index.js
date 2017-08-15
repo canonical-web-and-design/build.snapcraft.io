f1a8030dbca3e082a83317a2f26d396e8b7c9c8b
Richard McCartney <richard@mccartney.io>
2017-08-15 13:54:51 +0100

    Creating the initial icon component.

--- src/common/components/vanilla-modules/icons/index.js
+++ src/common/components/vanilla-modules/icons/index.js
@@ -2,3 +2,24 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
 import styles from '../../../style/vanilla/css/icons.css';
+
+export default function Icon (props) {
+  const { value, className, ...more } = props;
+
+  const iconClass = classNames(
+    styles[value],
+    className
+  );
+
+  return (
+    <i
+      className={iconClass}
+      {...more}
+    />
+  );
+}
+
+Icon.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string.isRequired
+};
