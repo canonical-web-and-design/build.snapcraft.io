22dea52a74f1584eb7a9776a4a7168d84bc9405f
Evan <evandandrea@users.noreply.github.com>
2017-10-06 10:43:42 -0400

    Directly embed the Google Optimize (and by extension Analytics) tags … (#964)
    * Directly embed the Google Optimize (and by extension Analytics) tags in the page, per https://support.google.com/360suite/optimize/answer/7359264
    
    * Make React JSX happy.
    
    * Backticks aren’t needed, there’s no variable sub
    
    * Correct Google Analytics
    
    * But really, correct Analytics
    
    * Set up the autoLinker
    
    * Fix autoLinker JS
    
    * Add missing auto to GA setup

--- src/server/helpers/html.js
+++ src/server/helpers/html.js
@@ -6,20 +6,56 @@ import { Provider } from 'react-redux';
 import { conf } from '../helpers/config';
 
 const GAID = conf.get('GOOGLE_ANALYTICS_ID');
+const OPTID = conf.get('GOOGLE_OPTIMIZE_ID');
+const GTMID = conf.get('GOOGLE_TAG_MANAGER_ID');
 
-const googleTagManager = GAID ?
+const googleOptimizePageHideCss = GAID && OPTID ?
+  <style
+    dangerouslySetInnerHTML={{ __html: '.async-hide { opacity: 0 !important}' }}
+  />
+  : null;
+
+const googleOptimizePageHide = GAID && OPTID ?
+  <script
+    dangerouslySetInnerHTML={{ __html: `
+    (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+    })(window,document.documentElement,'async-hide','dataLayer',4000,
+    {'${OPTID}':true});` }}
+  />
+ : null;
+
+const googleAnalytics = GAID && OPTID ?
+  <script
+    dangerouslySetInnerHTML={{ __html: `
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', '${GAID}', 'auto', {'allowLinker': true});
+    ga('require', 'linker');
+    ga('linker:autoLink', ['snapcraft.io', 'build.snapcraft.io', 'dashboard.snapcraft.io',
+    'conjure-up.io', 'login.ubuntu.com', 'www.ubuntu.com', 'ubuntu.com', 'insights.ubuntu.com',
+    'developer.ubuntu.com', 'cn.ubuntu.com', 'design.ubuntu.com', 'maas.io', 'canonical.com',
+    'landscape.canonical.com', 'pages.ubuntu.com', 'tutorials.ubuntu.com', 'docs.ubuntu.com']);
+    ga('require', '${OPTID}');` }}
+  />
+ : null;
+
+const googleTagManager = GTMID ?
   <script
     dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', '${GAID}')` }}
+})(window,document,'script','dataLayer', '${GTMID}')` }}
   /> : null;
 
-const googleTagManagerNoScript = GAID ?
+const googleTagManagerNoScript = GTMID ?
   <noscript>
     <iframe
-      src="https://www.googletagmanager.com/ns.html?id=${GAID}"
+      src="https://www.googletagmanager.com/ns.html?id=${GTMID}"
       height="0"
       width="0"
       style={{
@@ -43,6 +79,9 @@ export default class Html extends Component {
     return (
       <html {...attrs}>
         <head>
+          { googleOptimizePageHideCss }
+          { googleOptimizePageHide }
+          { googleAnalytics }
           { googleTagManager }
           {head.title.toComponent()}
           {head.meta.toComponent()}
