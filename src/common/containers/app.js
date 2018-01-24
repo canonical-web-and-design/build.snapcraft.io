3b354852c46c81bafc307f9805cb1eaa24d00fd6
Colin Watson <cjwatson@ubuntu.com>
2018-01-24 09:07:32 +0000

    Remove maintenance notification (#1053)
    The Launchpad build farm is mostly back to normal now.

--- src/common/containers/app.js
+++ src/common/containers/app.js
@@ -5,9 +5,6 @@ import Helmet from 'react-helmet';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import CookieNotification from '../components/cookie-notification';
-import Notification from '../components/vanilla-modules/notification';
-
-import styles from './container.css';
 
 export class App extends Component {
   render() {
@@ -26,14 +23,6 @@ export class App extends Component {
           user={this.props.user}
           dispatch={this.props.dispatch}
         />
-        {/* XXX: to be removed when maintenance is over */}
-        <div className={ styles.container }>
-          <Notification appearance='negative'>
-            Due to maintenance on the build farm over the past couple of days there is a possible delay in your builds being processed.<br/>
-            Apologies for the inconvenience and please bear with us while we work through the backlog.
-          </Notification>
-        </div>
-        {/* XXX */}
         { this.props.children }
         <Footer />
         <CookieNotification />
